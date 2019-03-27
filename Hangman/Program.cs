cd2ef37c706bc1a096774492d9f27ba0888585f8
Leonard Schubert <lschubert@1worldsync.com>
2019-03-27 16:45:04 +0100

    Liste auswählen

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,54 @@ namespace Hangman
 {
     class Program
     {
+        List<String> woerterListe = new List<String>();
+        List<String> buchstaben = new List<String>();
         static void Main(string[] args)
         {
+            SelectList();
+        }
+
+        //erst auswählen, welche Liste
+        public static void SelectList()
+        {
+            Console.WriteLine("Bitte Spielliste wählen:");
+            Console.WriteLine("1. Pokemon");
+            Console.WriteLine("2. Städte");
+
+            Boolean noInput = true;
+
+            while (noInput)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey();
+
+
+                if (cki.Key == ConsoleKey.D1)
+                {
+                    ReadFile("Hangman_Wortliste_Pokemon.txt");
+                    noInput = false;
+                }
+                else if (cki.Key == ConsoleKey.D2)
+                {
+                    ReadFile("Hangman_Wortliste_Städte.txt");
+                    noInput = false;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Bitte 1 oder 2 drücken");
+                }
+            }
+        }
+
+        //hier die Liste lesen
+        public static void ReadFile(string file)
+        {
+            string line;
+            StreamReader reader = new StreamReader(file);
+            while((line = reader.ReadLine()) != null){
+                woerterListe.Add(line);
+            }
+            Console.ReadLine();
         }
     }
 }
